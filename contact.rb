861ffc6ecc2ba9ea4c8d63f1558242dff6addf25
Nickolas Fuentes <nickfuentes24@gmail.com>
2017-11-06 13:02:20 -0600

    Contact names completed

---
+++ contact.rb
@@ -0,0 +1,71 @@
+class Contact
+  attr_writer :first_name, :middle_name, :last_name
+
+  def first_name
+    @first_name
+  end
+
+  def middle_name
+    @middle_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def first_last
+    first_name + " " + last_name
+  end
+
+  def last_first
+    last_first = last_name
+    last_first += ", "
+    last_first += first_name
+    if !@middle_name.nil?
+      last_first += " "
+      last_first += middle_name.slice(0,1)
+      last_first += "."
+    end
+    last_first
+  end
+
+  def full_name
+    full_name = first_name
+    if !@middle_name.nil?
+      full_name += " "
+      full_name += middle_name
+    end
+    full_name += " "
+    full_name += last_name
+    full_name
+  end
+
+  def to_s(format = "full_name")
+    case format
+    when "full_name"
+      full_name
+    when "last_first"
+      last_first
+    when "first"
+      first_name
+    when "last"
+      last_name
+    else
+      first_last
+    end
+  end
+end
+
+nick = Contact.new
+nick.first_name = "Nick"
+nick.middle_name = "Martin"
+nick.last_name = "Fuentes"
+nick.to_s
+puts nick.to_s("first_last")
+puts nick.to_s("full_name")
+
+jason = Contact.new
+jason.first_name = "Jason"
+jason.last_name = "Bourne"
+puts jason.to_s("full_name")
+puts jason.to_s("last_first")
